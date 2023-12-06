443dc8d6c9966c38fdf1c502ff6f6f98d4da566c
Kerpo <damien.riandiere@outlook.fr>
2023-12-06 10:14:40 +0100

    Add mock for tests

--- __tests__/services/authTests.ts
+++ __tests__/services/authTests.ts
@@ -1,22 +1,31 @@
 import dotenv from "dotenv";
 import { describe, it, expect, beforeAll, afterAll } from "@jest/globals";
 import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server-global";
 import logger from "../../src/utils/logger";
 
 import * as authService from "../../src/services/authService";
 import { deleteUser } from "../../src/services/userService";
 
+let mongoServer: any;
+
 describe("Test auth service", () => {
   beforeAll(async () => {
     dotenv.config();
-    if (process.env.DB_URL) {
-      await mongoose.connect(process.env.DB_URL, {});
+
+    mongoServer = await MongoMemoryServer.create();
+    const mongoUri = await mongoServer.getUri();
+
+    if (mongoUri) {
+      await mongoose.connect(mongoUri, {});
     } else {
-      logger.error("DB_URL is not defined");
+      logger.error("Failed to start MongoDB memory server");
     }
   });
+
   afterAll(async () => {
-    await mongoose.connection.close();
+    await mongoose.connection.close()
+    await mongoServer.stop()
   });
 
   it("Should be able to create a user.", async () => {
--- __tests__/services/userTests.ts
+++ __tests__/services/userTests.ts
@@ -1,46 +1,53 @@
-import dotenv from "dotenv";
-import { describe, it, expect, beforeAll, afterAll } from "@jest/globals";
-import mongoose from "mongoose";
-import logger from "../../src/utils/logger";
+import dotenv from "dotenv"
+import { describe, it, expect, beforeAll, afterAll } from "@jest/globals"
+import mongoose from "mongoose"
+import logger from "../../src/utils/logger"
+import { MongoMemoryServer } from "mongodb-memory-server-global"
 
-import * as userService from "../../src/services/userService";
-import { register } from "../../src/services/authService";
+import * as userService from "../../src/services/userService"
+import { register } from "../../src/services/authService"
+
+let mongoServer: any
 
 describe("Test user services", () => {
   beforeAll(async () => {
     dotenv.config();
-    if (process.env.DB_URL) {
-      await mongoose.connect(process.env.DB_URL, {
-      });
+
+    mongoServer = await MongoMemoryServer.create();
+    const mongoUri = await mongoServer.getUri();
+
+    if (mongoUri) {
+      await mongoose.connect(mongoUri, {});
     } else {
-      logger.error("DB_URL is not defined");
+      logger.error("Failed to start MongoDB memory server");
     }
   });
   afterAll(async () => {
     await mongoose.connection.close();
+    await mongoServer.stop();
   });
 
   it("Should be able to get a user profile.", async () => {
     const user = await register(
-      "unitTest2name",
-      "unitTest2@iamtest1.fr",
-      "unitTest2password"
+      "unitTest1name",
+      "unitTest1@iamtest1.fr",
+      "unitTest1password"
     );
-    logger.info("User registered !")
+    logger.info("User registered !");
     const userProfile = await userService.getUserProfile(user.userProfile.id);
-    logger.info("User profile retrieved !")
+    logger.info("User profile retrieved !");
 
-    expect(userProfile.name).toBe("unitTest2name");
-    expect(userProfile.email).toBe("unitTest2@iamtest1.fr");
+    expect(userProfile.name).toBe("unitTest1name");
+    expect(userProfile.email).toBe("unitTest1@iamtest1.fr");
 
     await userService.deleteUser(user.userProfile.id.toString());
   });
 
   it("Should be able to delete a user.", async () => {
     const user = await register(
-      "unitTest1name",
-      "unitTest1@iamtest1.fr",
-      "unitTest1password"
+      "unitTest2name",
+      "unitTest2@iamtest2.fr",
+      "unitTest2password"
     );
 
     const response = await userService.deleteUser(user.userProfile.id.toString());
